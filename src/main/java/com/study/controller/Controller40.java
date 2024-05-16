a7e9de0c6de65546174ee399ef110452f65d7e43
arbiterkdh <arbiterkdh@gmail.com>
2024-05-16 10:35:50 +0900

    /api/some-url4 의 get 요청처리메소드 연습

--- src/main/java/com/study/controller/Controller40.java
+++ src/main/java/com/study/controller/Controller40.java
@@ -1,5 +1,6 @@
 package com.study.controller;
 
+import com.study.domain.MyBean001;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,34 @@ public class Controller40 {
         System.out.println("name = " + name);
         System.out.println("email = " + email);
     }
+
+    @PostMapping("/api/some-url3")
+    public void method7(@RequestParam("id") Integer id,
+                        @RequestParam("city") String city,
+                        @RequestParam("country") String country) {
+        System.out.println("id = " + id);
+        System.out.println("city = " + city);
+        System.out.println("country = " + country);
+    }
+
+    // /api/some-url4 get, post 요청 처리 메소드 작성
+
+    @GetMapping("/api/some-url4")
+    public void method8(@RequestParam("id") Integer id,
+                        String name,
+                        String email) {
+        System.out.println("id = " + id);
+        System.out.println("name = " + name);
+        System.out.println("email = " + email);
+    }
+
+    @PostMapping("/api/some-url4")
+    public void method9(MyBean001 myBean001) {
+        System.out.println("myBean001 = " + myBean001.getId());
+        System.out.println("myBean001 = " + myBean001.getCity());
+        System.out.println("myBean001 = " + myBean001.getCountry());
+    }
+
 
     @GetMapping("")
     public void method5() {
---
+++ src/main/java/com/study/domain/MyBean001.java
@@ -0,0 +1,10 @@
+package com.study.domain;
+
+import lombok.Data;
+
+@Data
+public class MyBean001 {
+    private Integer id;
+    private String city;
+    private String country;
+}
